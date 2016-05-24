54437ec0149982c3f78b71344dd47c51d7683acc
Doreviateam <doreviateam@gmail.com>
2016-05-24 23:35:04 +0200

    chapitre introduction flask

--- hello.py
+++ hello.py
@@ -1,10 +1,39 @@
-from flask import Flask
+# -*- coding: utf-8 -*-
+from flask import Flask, request, redirect
+from flask import abort
+from flask.ext.script import Manager
+
 
 app = Flask(__name__)
+
+manager = Manager(app)
 
 @app.route("/")
 def index():
     return "<h1>Hello World!</h1>"
 
+@app.route("/agent")
+def agent():
+    user_agent = request.headers.get('User-Agent')
+    return "<p>Your brother is {}</p>".format(user_agent)
+
+@app.route("/user/<name>")
+def user(name):
+    return "<h2>Hello, {}!</h2>".decode("utf-8").format(name)
+
+@app.route('/re')
+def google():
+    return redirect('http://www.google.fr')
+
+@app.route('/er/<ids>')
+def get_user(ids):
+    er = "Michel PÖLAC"
+    ids = int(ids)
+    re = len(er) + ids
+    if not id:
+        abort(404)
+    return '{} + {} = {}'.encode("utf-8").format(er, ids, re)
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    manager.run()
+    # app.run(debug=True)
